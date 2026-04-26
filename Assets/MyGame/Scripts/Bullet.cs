453e7022db210e3e4219a6a103935c57a992fc1e
turcsekoliver <turcsek.oliver@gmail.com>
2026-04-26 21:39:56 +0200

    Gun script, bullet prefab, impact effect

--- Assets/MyGame/Scripts/Bullet.cs
+++ Assets/MyGame/Scripts/Bullet.cs
@@ -4,6 +4,9 @@ using UnityEngine;
  
 public class Bullet : MonoBehaviour
 {
+    public ParticleSystem explosionParticleSystem;
+    public AudioSource explosionAudioSource;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +17,20 @@ public class Bullet : MonoBehaviour
     void Update()
     {
          
+    }
+
+    // OnCollisionEnter is called when this collider/rigidbody
+    // has begun touching another rigidbody/collider.
+    public IEnumerator OnCollisionEnter(Collision collider)
+    {
+        Debug.Log("Bullet HIT!!!");
+
+        explosionParticleSystem.Play();
+        explosionAudioSource.Play();
+
+        GetComponent<SphereCollider>().enabled = false;
+        GetComponent<MeshRenderer>().enabled = false;
+
+        yield return null;
     }
 }
--- Assets/MyGame/Scripts/VRGun.cs
+++ Assets/MyGame/Scripts/VRGun.cs
@@ -5,6 +5,12 @@ using UnityEngine;
 public class VRGun : MonoBehaviour
 {
     public Transform gunSnapTransform;
+    public Transform bulletSpawnTransform;
+    public GameObject bulletPrefab;
+    public ParticleSystem gunFireParticleSystem;
+    public AudioSource gunFireAudioSource;
+
+    public float fireSpeed = 125.0f;
     
     // Start is called before the first frame update
     void Start()
@@ -16,6 +22,29 @@ public class VRGun : MonoBehaviour
     void Update()
     {
          
+    }
+
+    // Called by VRGun.XRGrabInteractable.InteractableEvents.Activated, when the gun is fired.
+    public void FireBullet()
+    {
+        Debug.Log("Gun is Fired!!!");
+
+        // Play the gunFireParticleSystem and gunFireAudioSource.
+        gunFireParticleSystem.Play();
+        gunFireAudioSource.Play();
+
+        // Spawn/Instantiate a clone of the Bullet-Prefab and store
+        // a reference to it in the GameObject spawnedBullet variable.
+        GameObject spawnedBullet = Instantiate(bulletPrefab);
+
+        // Position the spawnedBullet at the tip of the gun at the bulletSpawnTransform.position.
+        spawnedBullet.transform.position = bulletSpawnTransform.transform.position;
+        // Rotate the spawnedBullet at the tip of the gun at the bulletSpawnTransform.rotation.
+        spawnedBullet.transform.rotation = bulletSpawnTransform.transform.rotation;
+
+        // Get the spawnedBullet's Rigidbody component and set its velocity
+        // to the forward direction of the bullet multiplied by the fireSpeed.
+        spawnedBullet.GetComponent<Rigidbody>().linearVelocity = spawnedBullet.transform.forward * fireSpeed;
     }
 
     public void OnGrab()
