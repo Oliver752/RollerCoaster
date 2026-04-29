fa3e7e5e71519251e1ef671dc979ea447b7c70a5
SimonaPoliakova <poliakova27712@gmail.com>
2026-04-30 01:55:31 +0200

    Target prefab

--- Assets/MyGame/Scripts/Target.cs
+++ Assets/MyGame/Scripts/Target.cs
@@ -1,9 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
  
 public class Target : MonoBehaviour
 {
+    public ParticleSystem targetParticleSystem;
+    public AudioSource targetAudioSource;
+    public UnityEvent onTargetHitEvent;
+ 
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +19,28 @@ public class Target : MonoBehaviour
     void Update()
     {
          
+    }
+     
+    // OnCollisionEnter is called when this collider/rigidbody 
+    // has begun touching another rigidbody/collider.
+    public IEnumerator OnCollisionEnter(Collision collider)
+    {
+        Debug.Log("Target was hit by: " + collider.gameObject.name);
+ 
+        targetParticleSystem.Play();
+        targetAudioSource.Play();
+ 
+        // Disable the target's MeshCollider so it can not be hit multiple times.
+        GetComponent<MeshCollider>().enabled = false;
+        // Disable the target's MeshRenderer to turn it invisible
+        GetComponent<MeshRenderer>().enabled = false;
+     
+        if (onTargetHitEvent != null)
+        {
+            // If there are functions subscribed to the onTargetHitEvent invoke (call) them.
+            onTargetHitEvent.Invoke();
+        }
+         
+        yield return null;
     }
 }
