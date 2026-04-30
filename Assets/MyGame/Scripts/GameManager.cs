edd1036ec7603bc14708b4fc39eaca56258a539d
SimonaPoliakova <poliakova27712@gmail.com>
2026-04-30 02:54:20 +0200

    Tutorial canvas, game manager,

--- Assets/MyGame/Scripts/GameManager.cs
+++ Assets/MyGame/Scripts/GameManager.cs
@@ -1,18 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Splines;
  
 public class GameManager : MonoBehaviour
 {
+    public RollerCoasterTrain rollerCoasterTrain;
+    public GameObject tutorialCanvas;
+ 
     // Start is called before the first frame update
     void Start()
     {
- 
+        Debug.Log("The Game is Starting!!!");
     }
  
     // Update is called once per frame
     void Update()
     {
-         
+        Debug.Log("The Game is Running!!!");
+    }
+ 
+    public void StartRollerCoasterRide()
+    {
+        Debug.Log("The Ride is Starting!!!");
+ 
+        // Play the spline animation to start the ride.
+        rollerCoasterTrain.GetComponent<SplineAnimate>().Play();
+ 
+        // Deactivate the tutorial canvas when the player starts the ride.
+        tutorialCanvas.SetActive(false);
     }
 }
